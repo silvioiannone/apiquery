6fd635fd2b127d9d099239d771c37e822245d729
Silvio Iannone <silvioiannone91@gmail.com>
2017-02-13 22:59:01 +0100

    Remove validation because it’s badly implemented

--- src/APIQuery/Actions/Search.php
+++ src/APIQuery/Actions/Search.php
@@ -18,33 +18,10 @@ use SI\Laravel\APIQuery\Exceptions\InvalidQuery;
 class Search extends AbstractAction
 {
     /**
-     * Columns that can be queried.
-     *
-     * Use this array to specify the columns/fields that is possible to search for each model.
-     *
-     * @var array
-     */
-    protected $searchableColumns = [
-        'App\AccountingFirm' => ['name'],
-        'App\Company' => ['name'],
-        'App\Contact' => ['name'],
-        'App\Document' => ['name'],
-        'App\Event' => ['title'],
-        'App\Link' => ['name'],
-        'App\News' => ['title'],
-        'App\Report' => ['title'],
-        'App\Service' => ['title'],
-        'App\Support' => ['title'],
-        'App\User' => ['name']
-    ];
-
-    /**
      * @inheritdoc
      */
     public function handle()
     {
-        $this->validate();
-
         list($column, $value) = $this->getParameters();
 
         // The 'search' action can't work directly on collections because that would be like doing
@@ -88,26 +65,5 @@ class Search extends AbstractAction
     protected function getValue(): string
     {
         return $this->getParameters()[1] ?? '';
-    }
-
-    /**
-     * Validate whether the query can be performed or not.
-     */
-    protected function validate()
-    {
-        $subjectType = (new \ReflectionClass($this->subject))->getName();
-
-        if(!isset($this->searchableColumns[$subjectType]))
-        {
-            throw new InvalidQuery('The model ' . $subjectType . ' cannot be queried.');
-        }
-
-        $searchableColumns = $this->searchableColumns[$subjectType];
-
-        if(!in_array($this->getColumn(), $searchableColumns))
-        {
-            throw new InvalidQuery('Searches in the '. $this->getColumn() . ' column of the ' .
-                $subjectType . ' model are not allowed.');
-        }
     }
 }
